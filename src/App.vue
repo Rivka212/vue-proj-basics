c61bb2f3d26cdafa94ef05a19a1f4bb8f4dc2647
Bashari Rivka <basrivka1@gmail.com>
2025-02-03 00:39:05 +0200

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AnimalIndex from './cmps/AnimalIndex.vue'
+import SeasonClock from './cmps/SeasonClock.vue'
+import CountDown from './cmps/CountDown.vue'
+
+
+export default {
+  data() {
+    return {
+      txt: 'Hello Vue!'
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>{{ txt }}</h1>
+  <nav>
+    <button @click="route = 'AnimalIndex'">Animal</button>
+    <button @click="route = 'SeasonClock'">season-clock</button>
+    <button @click="route = 'CountDown'">count-down</button>
+  </nav>
+</template>
+
+<style scoped></style>
---
+++ src/cmps/SeasonClock.vue
@@ -0,0 +1,17 @@
+<template>
+  
+  </template>
+  
+  <script>
+  export default {
+    name: 'CountDown',
+    data() {
+      return {
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  

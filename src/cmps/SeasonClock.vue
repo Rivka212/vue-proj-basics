94af10c78e62da44ce2edc5679400be592fa6f68
Bashari Rivka <basrivka1@gmail.com>
2025-02-05 12:23:36 +0200

    Change SeasonClock cmp

--- src/cmps/SeasonClock.vue
+++ src/cmps/SeasonClock.vue
@@ -1,17 +1,95 @@
-<template>
-  
-  </template>
-  
-  <script>
-  export default {
-    name: 'CountDown',
+<script>
+
+export default {
+
     data() {
-      return {
-      };
+        return {
+            sectionStyle: '',
+            date: new Date(),
+            locale: 'en-US',
+            time: new Date().toLocaleTimeString(),
+            intervalId: null,
+            season: '',
+            monthNames: '',
+            day:'',
+        };
+    },
+
+    methods: {
+        toggleDarkMode() {
+            this.sectionStyle = this.sectionStyle === 'dark' ? '' : 'dark';
+        },
+        getDayName() {
+            return this.date.toLocaleDateString(this.locale, { weekday: 'long' });
+        },
+
+        getMonthName() {
+            const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December'];
+            return monthNames[this.date.getMonth()];
+
+        },
+
+        getSeason() {
+            const month = this.date.getMonth();
+            if (month === 11 || month <= 1) {
+                this.season = 'winter';
+            } else if (month >= 2 && month <= 4) {
+                this.season = 'spring';
+            } else if (month >= 5 && month <= 7) {
+                this.season = 'summer';
+            } else {
+                this.season = 'autumn'
+            }
+            return this.season
+        },
+
+
+        updateTime() {
+            this.date = new Date();
+            this.time = this.date.toLocaleTimeString();
+        },
+    },
+
+    mounted() {
+        this.day = this.getDayName(),
+        this.season = this.getSeason(),
+        this.monthNames = this.getMonthName(),
+
+        this.intervalId = setInterval(this.updateTime, 1000);
+    },
+    beforeDestroy() {
+        clearInterval(this.intervalId);
     }
-  };
-  </script>
-  
-  <style scoped>
-  </style>
-  
+}
+</script>
+
+<template>
+    <button @click="toggleDarkMode()" :class="['season-clock', sectionStyle]">
+        <p>{{ monthNames }}</p>
+        <p>({{ season }})</p>
+        <img :src="'./../assets/imgs/' + season + '.png'" alt="season-image">
+        <p>{{ day }}</p>
+        <p>{{ time }}</p>
+    </button>
+</template>
+
+<style scoped>
+.season-clock {
+    background-color: rgb(127, 191, 255);
+    padding: 30px;
+    margin: 20px;
+    font-size: 20px;
+    font-weight: 600;
+
+}
+
+img {
+    width: 20px;
+    height: 20px;
+}
+
+.dark {
+    background-color: rgb(27, 27, 96);
+}
+</style>
